eec68b017cdc82439491b63ba775966637a61812
54786 <54786@etu.he2b.be>
2022-01-10 07:21:19 +0100

    Test

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2021 Andrew SASSOYE, Constantin GUNDUZ, Gregory VAN DER PLUIJM,
+// Copyright (c) 2022 Andrew SASSOYE, Constantin GUNDUZ, Gregory VAN DER PLUIJM,
 // Thomas LEUTSCHER
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,10 +20,29 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+#include <QCoreApplication>
+#include <QHostAddress>
+#include <QString>
+#include <regex>
 
-#include <iostream>
+#include "server/tetris_server.hpp"
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+int main(int argc, char *argv[]) {
+  if (argc != 3) throw std::invalid_argument("invalid input from the start");
+  QCoreApplication app(argc, argv);
+
+  std::string regex = "^(?:[0-9]{1,3}.){3}[0-9]{1,3}$";
+  if (!std::regex_match(argv[1], std::regex(regex)))
+    throw std::invalid_argument("ip address is not valid");
+
+  QString IP = argv[1];
+  QHostAddress IPAddress = QHostAddress(IP);
+  int port = 0;
+
+  if (!std::isdigit(std::strtol(argv[2], nullptr, 10)) &&
+      (port = std::strtol(argv[2], nullptr, 10)) <= 0 && port >= 65537)
+    throw std::invalid_argument("port is not valid");
+  tetris::server::Tetris_Server server{&app, IPAddress, port};
+
+  return QCoreApplication::exec();
 }
